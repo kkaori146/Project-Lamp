c5a386b1672ef24f03e987f2f5477003fc55ad74
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-08 13:17:24 +0900

    onmouseout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 function acendeLampada() {
 window.document.getElementById('luz').src="./images/lampada-acesa.jpg";
 }
+function apagaLampada() {
+    window.document.getElementById('luz').src="./images/lampada-apagada.jpg";
+}
