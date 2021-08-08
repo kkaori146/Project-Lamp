dcc86b45ae8576823eb991d057070ba198ed882a
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-08 13:47:07 +0900

    Organize the Code (to simplify)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 var quebrada = false;
 
 function mudaLampada(tipo) {
-    if (tipo == 1) {
-        arquivo = "./images/lampada-acesa.jpg";
-    }
-    if (tipo == 2) {
-        arquivo = "./images/lampada-apagada.jpg";
-    }
-    if (tipo == 3) {
-        arquivo = "./images/lampada-quebrada.jpg";
 
-    }
     if(!quebrada) {
-window.document.getElementById('luz').src=arquivo;
-if (tipo == 3) {
+window.document.getElementById('luz').src = "./images/" + tipo + ".jpg";
+if (tipo == 'lampada-quebrada') {
     quebrada = true;
 }
 }
