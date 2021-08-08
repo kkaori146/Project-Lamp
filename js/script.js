705dd3ee171e8c65e898d0bb1b0dfbd6583dd824
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-08 13:34:58 +0900

    Rewriting the Code with one function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-function acendeLampada() {
-window.document.getElementById('luz').src="./images/lampada-acesa.jpg";
+var quebrada = false;
+
+function mudaLampada(tipo) {
+    if (tipo == 1) {
+        arquivo = "./images/lampada-acesa.jpg";
+    }
+    if (tipo == 2) {
+        arquivo = "./images/lampada-apagada.jpg";
+    }
+    if (tipo == 3) {
+        arquivo = "./images/lampada-quebrada.jpg";
+
+    }
+    if(!quebrada) {
+window.document.getElementById('luz').src=arquivo;
+if (tipo == 3) {
+    quebrada = true;
 }
-function apagaLampada() {
-    window.document.getElementById('luz').src="./images/lampada-apagada.jpg";
 }
-function quebraLampada() {
-    window.document.getElementById('luz').src="./images/lampada-quebrada.jpg";
 }
