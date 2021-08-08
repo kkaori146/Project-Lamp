1ab96a6668583d7b6b5a467c64bc083b14c539ed
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-08 13:20:34 +0900

    Broken Lamp

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ window.document.getElementById('luz').src="./images/lampada-acesa.jpg";
 function apagaLampada() {
     window.document.getElementById('luz').src="./images/lampada-apagada.jpg";
 }
+function quebraLampada() {
+    window.document.getElementById('luz').src="./images/lampada-quebrada.jpg";
+}
